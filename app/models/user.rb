b2df7d0c77532694fc5081973818998ca25a81eb
PyCreatorr <ttptv@yahoo.com>
2023-11-04 06:43:00 +0100

    Create new stripe user and assign/save stripe_user_id to the new created devise user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def to_s
+    email
+  end
+
+  after_create do
+    stripe_customer = Stripe::Customer.create( email: self.email )
+    update(stripe_customer_id: stripe_customer.id)
+  end
 end
