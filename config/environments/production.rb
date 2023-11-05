fbc96e033b7223fece0800b474f0a3b0c9e805a8
PyCreatorr <ttptv@yahoo.com>
2023-11-05 21:50:48 +0100

    production stripe changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,10 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'https://rails-7-shoplify-e563576662a5.herokuapp.com', :protocol => 'https' }
+  config.action_mailer.default_options = {:from => "support@devekma.fun"}
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,4 +1,5 @@
-Stripe.api_key = Rails.application.credentials.stripe.secret
+#Stripe.api_key = Rails.application.credentials.stripe.secret
+Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret)
 
 # the same Rails.application.credentials.stripe[:secret]
 # Rails.application.credentials[:stripe][:secret]
