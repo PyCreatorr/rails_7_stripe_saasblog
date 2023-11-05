1686a94e7f90290a87e33113c2de4973fc8dca46
PyCreatorr <ttptv@yahoo.com>
2023-11-05 12:06:20 +0100

    create product good recurring: monthly + yearly and show on the pricing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+
   devise_for :users
   root "posts#index"
   resources :posts
-
+  
+  get "pricing", to: "static_pages#pricing"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
